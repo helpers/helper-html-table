47e4ee724bfc5a7f8e56a3c808a4fd96456d0bd4
doowb <brian.woodward@gmail.com>
2016-12-22 21:13:13 -0500

    add documentation comments and additional documentation to .verb.md file

--- index.js
+++ index.js
@@ -2,6 +2,53 @@
 
 var utils = require('./utils');
 
+/**
+ * Create an html table from a JSON object.
+ *
+ * ```js
+ * var table = {
+ *   // attributes to add to the <table> tag
+ *   attr: 'class="table"',
+ *
+ *   // thead rows (each row has an array of columns)
+ *   thead: [['Foo', 'Bar', 'Baz']],
+ *
+ *   // tbody rows (each row has an array of columns)
+ *   tbody: [
+ *     ['foo', 'bar', 'baz'],
+ *     ['FOO', 'BAR', 'BAZ']
+ *   ]
+ * };
+ *
+ * var html = htmlTable(table);
+ * console.log(html);
+ * //=> <table class="table">
+ * //=>   <thead>
+ * //=>     <tr>
+ * //=>       <td>Foo</td>
+ * //=>       <td>Bar</td>
+ * //=>       <td>Baz</td>
+ * //=>     </tr>
+ * //=>   </thead>
+ * //=>   <tbody>
+ * //=>     <tr>
+ * //=>       <td>foo</td>
+ * //=>       <td>bar</td>
+ * //=>       <td>baz</td>
+ * //=>     </tr>
+ * //=>     <tr>
+ * //=>       <td>FOO</td>
+ * //=>       <td>BAR</td>
+ * //=>       <td>BAZ</td>
+ * //=>     </tr>
+ * //=>   </tbody>
+ * //=> </table>
+ * ```
+ * @param  {Object} `table` object containing properties describing the rows and columns in a table head and body. See [formats](#formats) for additional options.
+ * @return {String} HTML table generated from configuration object.
+ * @api public
+ */
+
 module.exports = function(table) {
   if (typeof table === 'undefined') {
     throw new Error('expected first argument to be an object');
